31b48cba0d1ab9d4d7a1f0f087e3dc916559484f
Lucca Silva <luccareinehr@gmail.com>
2023-01-13 22:08:16 +0100

    Change Ram address to 8 bits (4 KB)

--- Ram.vhd
+++ Ram.vhd
@@ -9,7 +9,7 @@ entity ram is
 			rw,en		:	in std_logic;
 			clk		:	in std_logic;
 			rst		:	in std_logic;
-			Adress	:	in std_logic_vector(7 downto 0);  -- 12 bits address, meaning a 4096  registers long memory
+			Adress	:	in std_logic_vector(7 downto 0); -- 8 bits address, meaning 128 rows = 4 kb
 			Data_in	:	in signed(31 downto 0); -- Data is defined here as being 32 bits long
 			Data_out:	out signed(31 downto 0) -- Data is defined here as being 32 bits long
 			);
@@ -20,8 +20,6 @@ architecture ram_a of ram is
 type ram is array(0 to 256) of signed(31 downto 0);
 
 signal Data_Ram : ram ;
-
-
 
 --------------- BEGIN -----------------------------------------------------------------
 begin
