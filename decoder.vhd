abcb9984caf0ee5d90d27e0a40374a8b3aea7c39
unknown <ana-luiza.bezerra@student-cs.fr>
2023-01-16 17:02:58 +0100

    Begin of decoder (misses Format 4)

--- decoder.vhd
+++ decoder.vhd
@@ -21,7 +21,7 @@ entity decoder is
 			PC_Jump	        :	out std_logic_vector(7 downto 0);	-- In case of a JUMP operation, specifies the destination
 
             EnRegFile, EnRAM, EnROM     :   out std_logic;
-            rwRegFile, rwRAM            :   out std_logic;
+            rRegFile, wRegFile, rwRAM            :   out std_logic;
             AddrRA, AddrRB  :   out std_logic_vector(4 downto 0); -- Addresses of RA and RB in the RegisterFile (read)
             AddrRdest : out std_logic_vector(4 downto 0) -- Address of Rdest in the RegisterFile (store)
             );
@@ -44,33 +44,96 @@ begin
         if rising_edge(clk) then
             -- Synchronous outputs
             case(Format) is
-                when "00" =>
-                    AddrRdest <= Instruction(10 downto 6);
-                    AddrRA <= Instruction(15 downto 11);
-                    AddrRB <= Instruction(20 downto 16);
+                when "00" => -- Format using 2 register datas for computations
+                    if Operator = "1001" then -- Case instruction is NOP
+                        NULL; -- See further after
                     
-                    case(Operator) is
-                        -- todo
-                    end case;
-                when "01" =>
-                    AddrRdest <= Instruction(10 downto 6);
-                    AddrRA <= Instruction(15 downto 11);
-                    Imm <= Instruction(28 downto 16);
+                    elsif Operator = "1000" then -- Case instruction is CLR
 
-                    case(Operator) is
-                        -- todo
-                    end case;
+                        -- Setting up addresses of writing and reading
+                        AddrRdest <= Instruction(10 downto 6);
+
+                        -- Setting up control signals for MUXs, ALU and RegisterFile
+                        SelOp <= Operator; -- Needs flip-flop (second cicle)
+                        SelMuxRam <= '0';
+                        rRegFile <= '0';
+                        wRegFile <= '1'; -- Needs flip-flop (second cicle)
+
+                        -- Enable of components
+                        EnRegFile <= '1';
+                        EnRAM <= '0';
+
+                    else
+
+                        -- Setting up addresses of writing and reading
+                        AddrRdest <= Instruction(10 downto 6);
+                        AddrRA <= Instruction(15 downto 11);
+                        AddrRB <= Instruction(20 downto 16);
+                        
+                        -- Setting up control signals for MUXs, ALU and RegisterFile
+                        SelOp <= Operator; -- Needs flip-flop (second cicle)
+                        SelMuxRam <= '0';
+                        SelMuxImm <= '0';
+                        rRegFile <= '1';
+                        wRegFile <= '1'; -- Needs flip-flop (second cicle)
+
+                        -- Enable of components
+                        EnRegFile <= '1';
+                        EnRAM <= '0';
+                    
+                    end if;
+
+                when "01" => -- Format using 1 register data and 1 immediate for computations
+
+                    if Operator = "1001" then -- Case instruction is NOP
+                        NULL; -- See further after
+
+                    elsif Operator = "1000" then -- Case instruction is CLR
+
+                        -- Setting up addresses of writing and reading
+                        AddrRdest <= Instruction(10 downto 6);
+
+                        -- Setting up control signals for MUXs, ALU and RegisterFile
+                        SelOp <= Operator; -- Needs flip-flop (second cicle)
+                        SelMuxRam <= '0';
+                        rRegFile <= '0';
+                        wRegFile <= '1'; -- Needs flip-flop (second cicle)
+
+                        -- Enable of components
+                        EnRegFile <= '1';
+                        EnRAM <= '0';    
+
+                    else
+
+                        -- Setting up addresses of writing and reading
+                        AddrRdest <= Instruction(10 downto 6);
+                        AddrRA <= Instruction(15 downto 11);
+                        Imm <= resize(Instruction(28 downto 16),32);
+
+                        -- Setting up control signals for MUXs, ALU and RegisterFile
+                        SelOp <= Operator; -- Needs flip-flop (second cicle)
+                        SelMuxRam <= '0';
+                        SelMuxImm <= '1';
+                        rRegFile <= '1';
+                        wRegFile <= '1'; -- Needs flip-flop (second cicle)
+
+                        -- Enable of components
+                        EnRegFile <= '1';
+                        EnRAM <= '0';
+
+                    end if;
+                    
                 when "10" =>
                     -- not used
                     -- only here to avoid latch
-                    abc
-                when "11" =>
+                    NULL;
+                
+                when "11" => 
                     AddrRAM <= Instruction(13 downto 6);
                     AddrRA <= Instruction(18 downto 14);
                     AddrRB <= Instruction(23 downto 19);
 
                     case(Operator) is
-                        -- todo
                     end case;
             end case;
 
