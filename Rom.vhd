216a87e6a75a412a409d253a52a1ca90ff6cccee
Vinicius Moraes <tmmvinicius@gmail.com>
2023-01-19 15:00:02 +0100

    Ignored the folder output_files generated by quartus. The Rom.vhd file has been updated in order to solve sintax errors. It now identifies the format of the instruction, and the output logic is ready. The instructions must still be loaded, and the block must be tested

--- Rom.vhd
+++ Rom.vhd
@@ -19,32 +19,48 @@ architecture rom_a of rom is
 type rom is array(0 to 31) of std_logic_vector(31 downto 0);
 
 signal Data_Rom : rom ;
-
+signal current_data : std_logic_vector(31 downto 0);
+signal format : std_logic_vector(1 downto 0);
 --------------- BEGIN -----------------------------------------------------------------
 begin
 
+	-- Definition of the instructions saved on the ROM
 
 
 
 
--- Saves NOP to all the other instructions
-	for k in (5) to 31 loop
-		Data_Rom(k) <= (others=>'00100100000000000000000000000000'); -- Encode the Do Nothing instruction PRESTAR ATENCAO NA ORDEM INVERTIDA DOS BITS
-	end loop;
+	-- Saves NOP to all the other instructions. Must define the proper starting number
+
+-- 	Output ROM logic. 
 
 	acces_rom:process(clk)
 		begin
 		
-			if rising_edge(clk) then
-				if en='1'then
-					if Data_Rom(to_integer(unsigned(Adress)))(1 downto 0) = "10" then -- Verifies if the format is 01 (we need to invert)
-						Data_out <= Data_Rom(to_integer(unsigned(Adress)))()
-					
-					Data_out <= Data_Rom(to_integer(unsigned(Adress)));
-				end if;
+		for k in 5 to 31 loop
+			Data_Rom(k) <= "00100100000000000000000000000000"; -- Encode the Do Nothing instruction. Must update the instruction for do nothing, considering the new ISA ordering
+		
+		end loop;
+		
+		if rising_edge(clk) then
+			if en='1'then
+				current_data <= Data_Rom(to_integer(unsigned(Adress)));
+				format <= current_data(1 downto 0);
 
+				if format = "01" then -- Verifies if the format is 01, which implies the use of immediate values (switches)
+					Data_out(15 downto 0) <= current_data(15 downto 0);
+					Data_out(24 downto 16) <= Switches(8 downto 0);
+					Data_out(31 downto 25) <= "0000000"; -- We only have 9 switches, which implies that the immediate values can only have 9 bits
+				else 
+					Data_out <= current_data;
+
+				end if;
 			end if;
+
+		end if;
 		
 	end process acces_rom;
 
 end rom_a;
+
+
+
