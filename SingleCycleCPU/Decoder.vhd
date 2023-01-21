b0068dd049a087af084679f07e7248bcc6f4b506
Lucca Silva <luccareinehr@gmail.com>
2023-01-21 15:52:33 +0100

    beq (branch if equal) working

--- SingleCycleCPU/Decoder.vhd
+++ SingleCycleCPU/Decoder.vhd
@@ -12,7 +12,7 @@ entity Decoder is
 	);
 	port(
 		-- inputs
-		Status: in std_logic_vector(2 downto 0); -- ALU status (3 bits: CarryOut, isZero, isPositive)
+		Status: in std_logic_vector(2 downto 0); -- ALU status (3 bits: (0) CarryOut, (1) isZero, (2) isPositive)
 		PC_now: in std_logic_vector(ROM_addr_width-1 downto 0);
 		Instr: in std_logic_vector(31 downto 0); -- instruction from ROM
 		SelOp: out std_logic_vector(3 downto 0); -- ALU select operation
@@ -22,7 +22,8 @@ entity Decoder is
 		selImm: out std_logic;
 		selRAM: out std_logic;
 		we_RegFile: out std_logic;
-		we_RAM: out std_logic
+		we_RAM: out std_logic;
+		PC_Load: out std_logic
 	);
 end entity Decoder;
 
@@ -46,6 +47,8 @@ begin
 	decode: process(all) begin
 		case(Format) is
 			when "00" => -- Rdest, RA[, RB]
+				PC_Load <= '0';
+				selImm <= '0';
 				if (Operator = "0000") or -- ADD
 					(Operator = "0001") or -- SUB
 					(Operator = "0010") or -- AND
@@ -60,7 +63,6 @@ begin
 					AddrRdest <= Instr(10 downto 6);
 					AddrRA <= Instr(15 downto 11);
 					AddrRB <= Instr(20 downto 16);
-					selImm <= '0';
 					selRAM <= '0';
 					we_RegFile <= '1';
 					we_RAM <= '0';
@@ -70,6 +72,8 @@ begin
 					we_RAM <= '0';
 				end if;
 			when "01" => -- Rdest, RA, Imm
+				PC_Load <= '0';
+				selImm <= '1';
 				if (Operator = "0000") or -- ADDI
 					(Operator = "0001") or -- SUBI
 					(Operator = "0010") or -- ANDI
@@ -82,7 +86,6 @@ begin
 					AddrRdest <= Instr(10 downto 6);
 					AddrRA <= Instr(15 downto 11);
 					Imm12 <= Instr(27 downto 16);
-					selImm <= '1';
 					selRAM <= '0';
 					we_RegFile <= '1';
 					we_RAM <= '0';
@@ -91,7 +94,6 @@ begin
 					AddrRA <= Instr(15 downto 11); -- RAM address
 					Imm12 <= Instr(27 downto 16); -- offset
 					SelOp <= "0000"; -- add RAM address and offset
-					selImm <= '1';
 					selRAM <= '1';
 					we_RegFile <= '1';
 					we_RAM <= '0';
@@ -100,11 +102,29 @@ begin
 					AddrRA <= Instr(15 downto 11); -- RAM address
 					Imm12 <= Instr(27 downto 16); -- offset
 					SelOp <= "0000"; -- add RAM address and offset
-					selImm <= '1';
 					we_RegFile <= '0';
 					we_RAM <= '1';
 				end if;
 						
+			when "11" => -- [RA, RB,] fonction 
+				if (Operator = "0000") then -- BEQ
+					SelOp <= "0001"; -- subtract RA from RB
+					we_RegFile <= '0';
+					we_RAM <= '0';
+					selImm <= '0';
+					AddrRB <= Instr(10 downto 6);
+					AddrRA <= Instr(15 downto 11);
+					Imm12 <= Instr(27 downto 16); -- ROM address of the branching function
+					
+					-- combinational statement to define PC_Load before next clock rising edge
+					if Status(1) = '1' then
+						-- Status(1) = isZero
+						PC_Load <= '1';
+					else
+						PC_Load <= '0';
+					end if;
+				end if;
+			
 			when others =>
 				SelOp <= Operator;
 				AddrRdest <= Instr(10 downto 6);
