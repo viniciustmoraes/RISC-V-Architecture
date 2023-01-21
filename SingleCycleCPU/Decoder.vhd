4899e313049937f2f80a23b7c3aebdca0dea2bc8
Lucca Silva <luccareinehr@gmail.com>
2023-01-21 04:02:42 +0100

    working SW and LW instructions + other ALU instructions

--- SingleCycleCPU/Decoder.vhd
+++ SingleCycleCPU/Decoder.vhd
@@ -20,6 +20,7 @@ entity Decoder is
 		AddrRA, AddrRB, AddrRdest: out std_logic_vector(4 downto 0);
 		Imm32: out std_logic_vector(31 downto 0);
 		selImm: out std_logic;
+		selRAM: out std_logic;
 		we_RegFile: out std_logic;
 		we_RAM: out std_logic
 	);
@@ -44,29 +45,66 @@ begin
 	
 	decode: process(all) begin
 		case(Format) is
-			when "00" => -- Rdest, RA, RB
-				if Operator = "0000" then
-					-- ADD
+			when "00" => -- Rdest, RA[, RB]
+				if (Operator = "0000") or -- ADD
+					(Operator = "0001") or -- SUB
+					(Operator = "0010") or -- AND
+					(Operator = "0011") or -- OR
+					(Operator = "0100") or -- XOR
+					(Operator = "0101") or -- NOT
+					(Operator = "0111") or -- MUL
+					(Operator = "1000") or -- CLR
+					(Operator = "1010") then -- MOV
+					
 					SelOp <= Operator;
 					AddrRdest <= Instr(10 downto 6);
 					AddrRA <= Instr(15 downto 11);
 					AddrRB <= Instr(20 downto 16);
 					selImm <= '0';
+					selRAM <= '0';
 					we_RegFile <= '1';
+					we_RAM <= '0';
+					
+				elsif (Operator = "1000") then -- NOP
+					we_RegFile <= '0';
 					we_RAM <= '0';
 				end if;
 			when "01" => -- Rdest, RA, Imm
-				if Operator = "0000" then
-					-- ADDI
+				if (Operator = "0000") or -- ADDI
+					(Operator = "0001") or -- SUBI
+					(Operator = "0010") or -- ANDI
+					(Operator = "0011") or -- ORI
+					(Operator = "0100") or -- XORI
+					(Operator = "1011") or -- SLLI
+					(Operator = "1100") then -- SRLI
+					
 					SelOp <= Operator;
 					AddrRdest <= Instr(10 downto 6);
 					AddrRA <= Instr(15 downto 11);
 					Imm12 <= Instr(27 downto 16);
 					selImm <= '1';
+					selRAM <= '0';
 					we_RegFile <= '1';
 					we_RAM <= '0';
+				elsif (Operator = "1101") then -- LW
+					AddrRdest <= Instr(10 downto 6);
+					AddrRA <= Instr(15 downto 11); -- RAM address
+					Imm12 <= Instr(27 downto 16); -- offset
+					SelOp <= "0000"; -- add RAM address and offset
+					selImm <= '1';
+					selRAM <= '1';
+					we_RegFile <= '1';
+					we_RAM <= '0';
+				elsif (Operator = "1110") then -- SW
+					AddrRB <= Instr(10 downto 6); -- value to be stored
+					AddrRA <= Instr(15 downto 11); -- RAM address
+					Imm12 <= Instr(27 downto 16); -- offset
+					SelOp <= "0000"; -- add RAM address and offset
+					selImm <= '1';
+					we_RegFile <= '0';
+					we_RAM <= '1';
 				end if;
-				
+						
 			when others =>
 				SelOp <= Operator;
 				AddrRdest <= Instr(10 downto 6);
