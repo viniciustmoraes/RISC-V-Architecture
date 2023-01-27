909669ffbaef938a3ded2a7b9f7d8cc67d0530e4
Vinicius Moraes <tmmvinicius@gmail.com>
2023-01-27 23:04:10 +0100

    Reescrita das instrucoes em formato compacto. Readaptacao da dinamica da ROM para funcionamento assincrono.

--- Rom.vhd
+++ Rom.vhd
@@ -6,8 +6,6 @@ Use ieee.numeric_std.all ;
 
 entity rom is
 	port(
-			en			:	in std_logic;
-			clk		:	in std_logic;
 			Adress	:	in std_logic_vector(4 downto 0); -- We choose 32 possible ROM instructions
 			Switches:	in std_logic_vector(8 downto 0); -- We take 9 immediate values from the board switches
 			Data_out:	out std_logic_vector(31 downto 0) -- We choose 32 bits for the instructions 
@@ -21,58 +19,47 @@ type rom is array(0 to 31) of std_logic_vector(31 downto 0);
 signal Data_Rom : rom ;
 signal current_data : std_logic_vector(31 downto 0);
 signal format : std_logic_vector(1 downto 0);
+signal operator : std_logic_vector(3 downto 0);
 --------------- BEGIN -----------------------------------------------------------------
 begin
 
 	-- Definition of the instructions saved on the ROM
 
-	-- Saves two immdiate values on the registers 0 (00000) and 1 (00001) from the register file
-	Data_Rom(0) <= "00000000000000001111100000000001";
-	Data_Rom(1) <= "00000000000000001111100001000001";
-
-	-- Operation or between the two saved values, and saves it two register number 5 (00101)
-	Data_Rom(2) <= "00000000000000000000100101001100";
-
-	-- Performs a NOT operation on the result of the previous operations, and saves it on register number 6 (00110)
-	Data_Rom(3) <= "00000000000000000010100110010100";
-
-	-- Moves the result from register 6 (00110) to register 7 (00111) and then clears register 6 (00110)
-	Data_Rom(4) <= "00000000000000000011000111101001";
-	
-	-- Clears the values stored on register 6
-	Data_Rom(5) <= "00000000000000000000000110100000";
-
-	-- Sends the values stored on register 7 to RAM, using the address 00000000
-	Data_Rom(6) <= "00000000000000011100000000011011";
+	Data_Rom(0) <= 32b"00000_00100_0000_01"; 		-- ADDI 0x04 0x00 Imm
+	Data_Rom(1) <= 32b"00000_00101_0000_01"; 		-- ADDI 0x05 0x00 Imm
+	Data_Rom(2) <= 32b"00101_00100_00110_0011_00";	-- OR 0x06 0x04 0x05
+	Data_Rom(3) <= 32b"00110_00111_0101_00"; 		-- NOT 0x07 0x06
+	Data_Rom(4) <= 32b"00111_01001_1010_00";		-- MOV 0x09 0x07 
+	Data_Rom(5) <= 32b"00111_1000_00"; 				-- CLR 0x07
+	Data_Rom(6) <= 32b"00000_01001_1101_01";  		-- LW 0x09 0x00 
 
 	-- Saves NOP to all the other instructions
 
 	generate_nop: for i in 7 to 31 generate
-		Data_Rom(i) <= "00000000000000000000000000100100";
+		Data_Rom(i) <= 32b"1001_00"; 				-- NOP
 	end generate;
 
 -- 	Output ROM logic. 
 
-	acces_rom:process(clk)
+	acces_rom:process(all)
 		begin
 		
 		-- Output logic
-		if rising_edge(clk) then
-			if en='1'then
-				current_data <= Data_Rom(to_integer(unsigned(Adress)));
-				format <= current_data(1 downto 0);
 
-				if format = "01" then -- Verifies if the format is 01, which implies the use of immediate values (switches)
-					Data_out(15 downto 0) <= current_data(15 downto 0);
-					Data_out(24 downto 16) <= Switches;
-					Data_out(31 downto 25) <= "0000000"; -- We only have 9 switches, which implies that the immediate values can only have 9 bits
-				else 
-					Data_out <= current_data;
+		current_data <= Data_Rom(to_integer(unsigned(Adress)));
+		format <= current_data(1 downto 0);
+		operator <= current_data(5 downto 2);
 
-				end if;
-			end if;
+		if (format = "01") and (operator /= "1101") and (operator /= "1110") then -- Verifies if the format is 01, which implies the use of immediate values (switches)
+			Data_out(15 downto 0) <= current_data(15 downto 0);
+			Data_out(24 downto 16) <= Switches;
+			Data_out(31 downto 25) <= "0000000"; -- We only have 9 switches, which implies that the immediate values can only have 9 bits
+		
+		else
+			Data_out <= current_data;
 
 		end if;
+
 		
 	end process acces_rom;
 
