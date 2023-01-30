97de3f425378d484109dfe54d8122fd5f26fe219
Lucca Silva <luccareinehr@gmail.com>
2023-01-30 22:10:09 +0100

    Reorganize repo folders and add README

--- Fetch.vhd
+++ Fetch.vhd
@@ -3,64 +3,50 @@ library ieee;
 use IEEE.STD_LOGIC_1164.ALL;
 Use ieee.numeric_std.all ;
 
-
 entity Fetch is
+	generic(
+			ROM_addr_width: integer := 5
+			-- ROM address width, in bits
+			-- determines the number of rows in the ROM (2^addr_width)
+			-- example: width=8 --> 256 rows
+			);
 	port(
 			en			:	in std_logic;
 			clk		:	in std_logic;
 			rst		:	in std_logic;
-			PC_load	:	in std_logic;	-- "Enable" for jump functions. Must come from the decoder
-			PC_Jump	:	in std_logic_vector(7 downto 0);	-- Jump address. Comes from the decoder
-			PC_out	:	out std_logic_vector(7 downto 0)	-- Final program counter, outputed by the fetch
+			PC_Load	:	in std_logic;	-- "Enable" for jump functions. Must come from the decoder
+			PC_Jump	:	in std_logic_vector(ROM_addr_width-1 downto 0);	-- Jump address. Comes from the decoder
+			PC_Out	:	out std_logic_vector(ROM_addr_width-1 downto 0)	-- Final program counter, output from fetch block
 			);
 end Fetch;
 
-
 architecture Fetch_a of Fetch is
 
-signal PC_counter: std_logic_vector(7 downto 0);
-
-Begin
-
-
-Process (clk, rst)
-
+signal PC_counter: std_logic_vector(ROM_addr_width-1 downto 0);
 
 begin
 
-	
-	if rst='1' then
-	
-		PC_counter<= (others=>'0');
-	
-	else
-	
-		If rising_edge(clk) then
-			if en='1' then
-			
-				If PC_Load='0' then
-				
-					PC_counter<=std_logic_vector(unsigned(PC_counter)+1);
-				
-				else
-				
-					PC_counter<=PC_Jump;
-					
+	process (clk, rst) begin
+		if rst='1' then
+			PC_counter <= (others=>'0');
+		else
+			if rising_edge(clk) then
+				if en='1' then
+					if PC_Load='1' then
+					-- branch to jump address
+						PC_counter <= PC_Jump;
+					else
+					-- prepare next instruction
+						PC_counter <= std_logic_vector(unsigned(PC_counter)+1);
+					end if;
 				end if;
-				
 			end if;
-			
 		end if;
-		
-	end if;
-	
-end Process;
+	end process;
 
+	PC_out <= PC_counter;
 
-PC_out <= PC_counter;
-
-
-end Architecture Fetch_a;
+end architecture Fetch_a;
 
 	
 			
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# RISC-V-Architecture
-Development of a RISC V architecture VHDL description
+# CentraleSupélec Digital Systems and Computer Architecture Group Project
+## 32-bit Soft-Core CPU
+
+This repository contains the code for a 32-bit soft-core CPU, created as a group project for the course of Digital Systems and Computer Architecture at CentraleSupélec.
+
+The goal of this project was to implement a subset of the RV32IM architecture (RISC-V), described using VHDL and the Intel Quartus Prime software. However, we did not follow the machine code format from RISC-V due to time constraints and a lack of familiarity with the architecture at the time.
+
+Manual verification was carried out using ModelSim and the Terasic DE10-Lite board.
+
+### Proposed Improvements
+
+- Implementing the complete RV32IM architecture and machine code format.
+- Adding more test cases to thoroughly test the CPU.
+- Reviewing the code for better readability and maintainability.
+
+### Authors
+
+- Ana Luiza Haas Bezerra
+- Lucca Reinehr Silva
+- Vinícius Teixeira Magalhães de Moraes
+
+### Acknowledgments
+
+- The instructors and TA's of the CentraleSupélec Digital Systems and Computer Architecture course for their guidance and support during this project.
